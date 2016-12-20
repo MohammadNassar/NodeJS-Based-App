d780eedf9a0177ebc789c871e9fa0e5e6e5f2eb2
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2016-12-20 18:13:28 +0000

    Writing an index page with a FORM that uses the GET method, and then retrieving the input data submitted via the form. Finally, storing and displaying the input data in JSON format.

--- server.js
+++ server.js
@@ -20,6 +20,20 @@ app.get('/ab*cd', function(req, res) {
 // The following line allows access to static files, e.g. '/images/bg.png'
 app.use(express.static('public'));
 
+app.get('/index', function(req, res) {
+	res.sendFile(__dirname + '/index.htm');
+});
+
+app.get('/process_form_get', function(req, res) {
+	// Create a response in JSON format
+	response = {
+		first_name : req.query.first_name,
+		last_name : req.query.last_name
+	};
+	console.log(response);
+	res.end(JSON.stringify(response));
+});
+
 var server = app.listen(8081, function () {
 	var host = server.address().address;
 	var port = server.address().port;
