93759527d667b39912476201c50e34b47b07b25b
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2016-12-20 17:13:09 +0000

    Using 'express.static' to serve/render static files such as: images, CSS, javascript, etc.

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.get('/list_user', function(req, res) {
 
 app.get('/ab*cd', function(req, res) {
 	console.log('Received a GET request for page/s \'ab*cd\'');
+	res.send('Page Pattern Matched (Using a Regular Expression)');
 });
+
+// The following line allows access to static files, e.g. '/images/bg.png'
+app.use(express.static('public'));
 
 var server = app.listen(8081, function () {
 	var host = server.address().address;
